0297ea00ebbbb50fc1a622f99b233728ce6affd1
steve2482 <stevecb12684@yahoo.com>
2017-04-01 18:31:24 -0400

    replacing ensureAuthenticated that was removed in error to try to use authenicationMilleware in it's place

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,23 @@ const authenticationMiddleware = require('../middleware/authenticationMiddleware
 
 // Get Homepage========================================================
 // ====================================================================
-router.get('/', authenticationMiddleware, function(req, res) {
+router.get('/', ensureAuthenticated, function(req, res) {
   let isLoggedIn = !!req.user;
   res.render('index', {loggedIn: isLoggedIn});
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/users/login');
+  }
+}
+
 // Check to see if a user is logged in=================================
 // ====================================================================
 router.get('/logged-in', function(req, res) {
-	let isLoggedIn = !!req.user;
+  let isLoggedIn = !!req.user;
   res.send({loggedIn: isLoggedIn});
 });
 
